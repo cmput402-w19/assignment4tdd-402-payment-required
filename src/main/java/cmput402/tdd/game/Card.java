1063fe37e4daeae81038869414a9dea46e08a25d
Justin Widney <jwidney@cs.ualberta.ca>
2019-02-25 22:13:42 -0700

    RED added Card & Deck Class.

---
+++ src/main/java/cmput402/tdd/game/Card.java
@@ -0,0 +1,29 @@
+package cmput402.tdd.game;
+
+
+public class Card {
+
+
+
+    private int cardNumber;
+
+
+
+
+    // constructor method
+    public Card (int number){
+
+        this.cardNumber  =0;
+
+
+    }
+
+
+
+    // Return the number of the card
+    public int getCardNumber(){
+
+        return 0;
+    }
+
+}
---
+++ src/main/java/cmput402/tdd/game/Deck.java
@@ -0,0 +1,39 @@
+package cmput402.tdd.game;
+
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import jdk.nashorn.internal.ir.WhileNode;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Deck {
+
+
+
+
+    private Card[] deck = new Card[52];
+    private boolean[] drawn = new boolean[52];
+    private int y;
+
+
+
+
+    public Deck(){
+
+
+    }
+
+    public Card drawCard(){
+
+        return null;
+
+
+    }
+
+
+
+
+
+
+
+}
